35e171933d73ce5ebbe5abf5b4787f244abfa271
godf1rey <godfreyjk8@gmail.com>
2024-09-21 08:21:40 +0300

    Add files via upload

---
+++ Godfrey.php
@@ -0,0 +1,16 @@
+<?php
+
+$name="MUGISHA GODFREY";
+$DateOfBirth="06/12/2002";
+$Address="LUWEERO";
+$MYear=2024;
+$GYear=2002;
+$Age=$MYear-$GYear;
+echo"Name: ".$name. "<br>";
+echo"Date Of Birth : ".$DateOfBirth."<br>";
+echo"Home Address: ".$Address."<br>";
+echo"Welcome home dear, ".$name."<br>";
+echo"You are now , ".$Age."Years old";
+/*MYear stands for current year
+GYear stands for year of birth*/
+?> 
